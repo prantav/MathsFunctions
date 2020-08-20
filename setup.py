9d0645c7e706eeb7b535452df5631f7ec6ac11cb
prantav <67049497+prantav@users.noreply.github.com>
2020-08-20 23:43:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-programmer_pogav", # Replace with your own username
+    name="MathsFunctions-programmer_pogav", # Replace with your own username
     version="0.0.1",
     author="Pranav Challa",
     author_email="pranav.moony.challa@gmail.com",
